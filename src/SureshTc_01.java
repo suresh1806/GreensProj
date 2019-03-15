672f268a7623a971b3aae6e064d97b52e8246c00
suresh1806 <suresh.csrao99@gmail.com>
2019-03-15 15:22:01 +0530

    added first test case

---
+++ src/SureshTc_01.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SureshTc_01 {
+	static{
+		System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe");
+	}
+	public static void main(String[] args) {
+		ChromeDriver driver=new ChromeDriver();
+
+		
+	
+	}
+
+}
