7ec44366fbc50e203acb00c6a340b0b1a9ed33cf
sivabalasundaram <sivabalasundaram@Sivambika>
2019-03-17 16:39:10 +0530

    sakthi

---
+++ src/browser.java
@@ -0,0 +1,12 @@
+
+
+public class browser {static{
+	System.setProperty("webdriver.chrome.driver" , "./Driver/chromedriver.exe");
+}
+public static void main(String[] args) throws InterruptedException {
+	//WebDriver driver=new ChromeDriver();
+	//driver.get("http://www.google.com");
+	//WebElement s = driver.findElement(By.name("q"));
+	//System.out.println(s.getText());
+}
+}
